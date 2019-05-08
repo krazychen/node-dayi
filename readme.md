ceac7a30067b20cd48db17ef2535a10ea3cfb6ee
krischen <krazy.chen@gmail.com>
2019-05-08 16:09:45 +0800

    修改部分title名称

---
+++ readme.md
@@ -0,0 +1,29 @@
+# 基于Node.js搭建的辅导答疑系统
+### 项目架
+##### 技术栈：
+
+Node.JS + Express(EJS)  + MySQL 
+
+### 开始
+```
+cd node-dayi/ # 进入目录
+npm install  # 安装依赖
+npm start  # 启动
+```
+打开浏览器访问 http://localhost:3000
+#### 文件结构
+```
+---- bin
+---- node_modules 依赖
+---- public  公共静态文件
+---- routes  路由
+---- server  服务端代码
+---- views  前端页面模版
+-------- common  通用前端代码
+-------- jie 问题前端代码
+-------- user 用户管理前端代码
+---- app.js  应用启动入口
+---- package.json 依赖描述
+---- package-lock.json
+---- readme.md
+```
--- server/dbhelper.js
+++ server/dbhelper.js
@@ -5,18 +5,21 @@ const dbConfig = config.my_dbConfig_master;
 // 创建数据池
 const pool  = mysql.createPool(dbConfig);
 
+// 创建写数据库连接
 function con_write() {
     let connection = mysql.createConnection(dbConfig);
     connection.connect();
     return connection;
 }
 
+// 创建读取专用的数据库连接
 function con_read() {
     let connection = mysql.createConnection(dbConfig);
     connection.connect();
     return connection;
 }
 
+// 创建通用的ES7数据库连接方式
 let query = function( sql, values ) {
     // 返回一个 Promise
     return new Promise(( resolve, reject ) => {
